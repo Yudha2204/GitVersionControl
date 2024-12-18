6ef84676ac9be8d1e31862e7274c479ee9dc796c
yudha1404 <inforsys_develop@inforsys.co.id>
2024-12-18 14:18:48 +0700

    add default urls when running

--- GitVersion/Program.cs
+++ GitVersion/Program.cs
@@ -21,6 +21,7 @@ namespace GitVersion
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:51000");
                 });
     }
 }
